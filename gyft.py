ccb204fc0d8b0e1eff3d6a6dc796beeca1bee81d
Aditya Vikram Singh <xypnox@gmail.com>
2018-03-11 17:41:15 +0530

    Can Now generate ICS within script :smile:

--- gyft.py
+++ gyft.py
@@ -1,5 +1,42 @@
 from bs4 import BeautifulSoup as bs
 import json
+import generate_ics
+
+import sys
+
+
+def query_yes_no(question, default="yes"):
+    """Ask a yes/no question via raw_input() and return their answer.
+
+    "question" is a string that is presented to the user.
+    "default" is the presumed answer if the user just hits <Enter>.
+        It must be "yes" (the default), "no" or None (meaning
+        an answer is required of the user).
+
+    The "answer" return value is True for "yes" or False for "no".
+    """
+    valid = {"yes": True, "y": True, "ye": True,
+             "no": False, "n": False}
+    if default is None:
+        prompt = " [y/n] "
+    elif default == "yes":
+        prompt = " [Y/n] "
+    elif default == "no":
+        prompt = " [y/N] "
+    else:
+        raise ValueError("invalid default answer: '%s'" % default)
+
+    while True:
+        sys.stdout.write(question + prompt)
+        choice = input().lower()
+        if default is not None and choice == '':
+            return valid[default]
+        elif choice in valid:
+            return valid[choice]
+        else:
+            sys.stdout.write("Please respond with 'yes' or 'no' "
+                             "(or 'y' or 'n').\n")
+
 
 with open('view_stud_time_table.html', 'r') as myfile:
     r = myfile.read()
@@ -104,7 +141,10 @@ print(
     '''
     Timetable saved to data.txt file. Be sure to edit this file,
     to have desired names of subjects rather than subject codes.
-
-    Also, run generate_ics.py to generate the ics file
     '''
 )
+
+if query_yes_no("Do you want to generate ICS file too?"):
+    generate_ics.main()
+else:
+    print("You can Generate ICS later using generate_ics.py")
