fa295972d40bca101e1de8e6e9f13dd0c3961f66
xypnox <xypnox@gmail.com>
2019-07-21 14:34:16 +0530

    Prettify code

--- Query_yn.py
+++ Query_yn.py
@@ -11,8 +11,7 @@ def query_yes_no(question, default="yes"):
 
     The "answer" return value is True for "yes" or False for "no".
     """
-    valid = {"yes": True, "y": True, "ye": True,
-             "no": False, "n": False}
+    valid = {"yes": True, "y": True, "ye": True, "no": False, "n": False}
     if default is None:
         prompt = " [y/n] "
     elif default == "yes":
@@ -25,10 +24,9 @@ def query_yes_no(question, default="yes"):
     while True:
         sys.stdout.write(question + prompt)
         choice = input().lower()
-        if default is not None and choice == '':
+        if default is not None and choice == "":
             return valid[default]
         elif choice in valid:
             return valid[choice]
         else:
-            sys.stdout.write("Please respond with 'yes' or 'no' "
-                             "(or 'y' or 'n').\n")
+            sys.stdout.write("Please respond with 'yes' or 'no' " "(or 'y' or 'n').\n")
--- dates.py
+++ dates.py
@@ -1,6 +1,7 @@
 from __future__ import print_function
 
 import build_event
+
 # import datetime
 # import pytz
 # SEM_BEGIN = datetime.datetime.now(pytz.timezone('Asia/Kolkata'))
@@ -16,10 +17,10 @@ END_TERM_BEGIN = build_event.generateIndiaTime(2019, 11, 18, 0, 0)
 # Sanity check
 
 sanity = [
-            SEM_BEGIN < MID_TERM_BEGIN,
-            MID_TERM_BEGIN < MID_TERM_END,
-            MID_TERM_END < END_TERM_BEGIN
-         ]
+    SEM_BEGIN < MID_TERM_BEGIN,
+    MID_TERM_BEGIN < MID_TERM_END,
+    MID_TERM_END < END_TERM_BEGIN,
+]
 
 # check if anything is False
 sanity_check = [item for item in sanity if not item]
@@ -28,14 +29,12 @@ if len(sanity_check) > 0:
     print("Check the dates you have entered")
     print("Note: SEM_BEGIN < MID_TERM_BEGIN < MID_TERM_END < END_TERM_BEGIN")
     import os
+
     os._exit(1)
 
 
 def get_dates():
-    '''
+    """
     Returns a list of lists denoting the time periods of working days
-    '''
-    return [
-                [SEM_BEGIN, MID_TERM_BEGIN],
-                [MID_TERM_END, END_TERM_BEGIN]
-           ]
+    """
+    return [[SEM_BEGIN, MID_TERM_BEGIN], [MID_TERM_END, END_TERM_BEGIN]]
